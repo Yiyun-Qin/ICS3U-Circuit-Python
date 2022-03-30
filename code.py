82b768055f5c302e512c8d684b3a736fddfe5850
Yiyun Qin <yiyun.qin@mths.ca>
2022-03-30 18:26:22 +0000

    hello world

---
+++ code.py
@@ -0,0 +1,23 @@
+# /usr/bin/env python3
+
+# Created by: Yiyun Qin
+# Created on: March 2022
+# This is the star wars game
+
+import ugame
+import stage
+
+
+def game_scene():
+    # main game function
+
+    print("\n\n\n")
+    print("Hello, World!")
+
+    # repeat forever, game loop
+    while True:
+        pass  # just a placeholder now
+
+
+if __name__ == "__main__":
+    game_scene()
