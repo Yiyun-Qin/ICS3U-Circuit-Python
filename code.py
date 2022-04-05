44782d4c56174dc614344ca90e16a8917fb48fd4
Yiyun-Qin <99287007+Yiyun-Qin@users.noreply.github.com>
2022-04-05 14:41:01 -0400

    sprite ship

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 # /usr/bin/env python3
 
 # Created by: Yiyun Qin
-# Created on: April 2022
+# Created in: April 2022
 # This is the star wars game
 
 import ugame
@@ -13,19 +13,30 @@ def game_scene():
 
     # grab the image, is image bank for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
     # 10 x 8 tiles for size 16 x 16
     background = stage.Grid(image_bank_background, 10, 8)
 
+    # pick the 5th picture and shown up at (75, 66)
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+
     # create a stage to display background, frame rate 60 fps
     game = stage.Stage(ugame.display, 60)
-    # set the layer for background
-    game.layers = [background]
+    # set the layer, items show up in order
+    game.layers = [ship] + [background]
     # take layers to show on the screen
     game.render_block()
 
     # repeat forever, game loop
     while True:
-        pass  # just a placeholder now
+        # get user input
+
+        # update game logic
+
+        # redraw Sprites
+        game.render_sprites([ship])
+        # wait until 60 tick and loop again
+        game.tick()
 
 
 if __name__ == "__main__":
