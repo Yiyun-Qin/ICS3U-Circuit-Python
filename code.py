d69d773d463ade803bb1a2f7380ac90b67818585
Yiyun-Qin <99287007+Yiyun-Qin@users.noreply.github.com>
2022-04-21 13:53:37 -0400

    menu

--- code.py
+++ code.py
@@ -18,14 +18,14 @@ def menu_scene():
     # add text objects
     text = []
     text1 = stage.Text(
-        width = 29, height = 12, font = None, palette = constants.RED_PALETTE, buffer = None
+        width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None
     )
     text1.move(20, 10)
     text1.text("MT Game Studios")
     text.append(text1)
 
     text2 = stage.Text(
-        width = 29, height = 12, font = None, palette = constants.RED_PALETTE, buffer = None
+        width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None
     )
     text2.move(40, 110)
     text2.text("PRESS START")
