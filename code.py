6b3c1aeca35559272584753960c5b97633c67fa7
Yiyun Qin <99287007+Yiyun-Qin@users.noreply.github.com>
2022-05-05 14:02:37 -0400

    hit

--- code.py
+++ code.py
@@ -286,10 +286,16 @@ def game_scene():
             if lasers[laser_number].x > 0:
                 for alien_number in range(len(aliens)):
                     if aliens[alien_number].x > 0:
-                        if stage.collide(lasers[laser_number].x + 6, lasers[laser_number].y + 2,
-                                         lasers[laser_number].x + 11, lasers[laser_number].y + 12,
-                                         aliens[alien_number].x + 1, aliens[alien_number].y,
-                                         aliens[alien_number].x + 15, aliens[alien_number].y + 15):
+                        if stage.collide(
+                            lasers[laser_number].x + 6,
+                            lasers[laser_number].y + 2,
+                            lasers[laser_number].x + 11,
+                            lasers[laser_number].y + 12,
+                            aliens[alien_number].x + 1,
+                            aliens[alien_number].y,
+                            aliens[alien_number].x + 15,
+                            aliens[alien_number].y + 15
+                        ):
 
                             # you hit an alien
                             aliens[alien_number].move(constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y)
